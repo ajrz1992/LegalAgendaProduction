9ee4a0f09e557e6641833f99fc3e4d75128456c8
Aldo Josue Rodriguez Zuniga <ajrz_1992@hotmail.es>
2024-11-01 00:53:30 -0600

    Actualizacion para visualizacion de opcion ver progreso en SideBar

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
         <?php if($_SESSION['login_type'] == 2): ?>
           <p class="card-text"><b>¡Bienvenido </b><?php echo $_SESSION['login_name'] ?>!</p>
         <?php elseif($_SESSION['login_type'] == 1): ?>
-        <!-- <h3 class="text-center p-0 m-0"><b>Evaluador</b></h3> -->
+        <h3 class="text-center p-0 m-0"><b>Evaluador</b></h3>
          <?php else: ?>
         <h3 class="text-center p-0 m-0"><b>Empleado</b></h3>
         <?php endif; ?>
@@ -31,6 +31,12 @@
               </p>
             </a>
             <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./index.php?page=progreso" class="nav-link nav-carpetaRaiz">
+                  <i class="right fas fa-angle-right"></i>
+                  <p> Ver Progreso</p>
+                </a>
+            </li>
             <li class="nav-item">
                 <a href="./index.php?page=carpetas" class="nav-link nav-carpetaRaiz">
                   <i class="fas fa-folder-minus"></i>
@@ -64,14 +70,14 @@
             </ul>
           </li>
           <?php if($_SESSION['login_type'] != 0): ?>
-          <!-- <li class="nav-item dropdown">
+          <li class="nav-item dropdown">
             <a href="./index.php?page=evaluation" class="nav-link nav-evaluation">
               <i class="nav-icon far fa-edit"></i>
               <p>
                 Evaluación
               </p>
             </a>
-          </li> -->
+          </li>
         <?php endif; ?>
           <?php if($_SESSION['login_type'] == 2): ?>
           <li class="nav-item dropdown">
@@ -94,7 +100,7 @@
             <a href="#" class="nav-link nav-edit_employee">
               <i class="nav-icon fas fa-user-friends"></i>
               <p>
-                Abogados
+                Empleados
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -102,18 +108,18 @@
               <li class="nav-item">
                 <a href="./index.php?page=new_employee" class="nav-link nav-new_employee tree-item">
                   <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Agregar Nuevo Abogado</p>
+                  <p>Agregar Nuevo Empleado</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="./index.php?page=employee_list" class="nav-link nav-employee_list tree-item">
                   <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Lista de Abogados</p>
+                  <p>Lista de Empleados</p>
                 </a>
               </li>
             </ul>
           </li>
-          <!-- <li class="nav-item">
+          <li class="nav-item">
             <a href="#" class="nav-link nav-edit_evaluator">
               <i class="nav-icon fas fa-user-secret"></i>
               <p>
@@ -135,7 +141,7 @@
                 </a>
               </li>
             </ul>
-          </li> -->
+          </li>
 
           <li class="nav-item">
             <a href="./index.php?page=calendario" class="nav-link nav-calendario">
@@ -165,7 +171,7 @@
             <a href="#" class="nav-link nav-edit_user">
               <i class="nav-icon fas fa-users"></i>
               <p>
-                Administradores
+                Usuarios
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -173,13 +179,13 @@
               <li class="nav-item">
                 <a href="./index.php?page=new_user" class="nav-link nav-new_user tree-item">
                   <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Agregar Nuevo Administrador</p>
+                  <p>Agregar Nuevo Usuario</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="./index.php?page=user_list" class="nav-link nav-user_list tree-item">
                   <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Lista de Administradores</p>
+                  <p>Lista de Usuarios</p>
                 </a>
               </li>
             </ul>
@@ -207,7 +213,7 @@
 				data: { nombre: dato }, // Aquí se pasa la variable "dato" como parte de los datos de la solicitud
 				success: function(response) {
 					console.log('Respuesta del servidor:', response);
-					window.location.href = 'https://demolegalagenda.tecnologiainnovacion.com/index.php?page=task_list';
+					window.location.href = 'http://localhost/LegalAgenda/index.php?page=task_list';
 				},
 				error: function(xhr, status, error) {
 					console.error('Error en la solicitud AJAX:', error);
